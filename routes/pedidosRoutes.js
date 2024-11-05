a0697e90f7033787f088c906cb8fccc471b00a88
Sergi Florensa <sergiflorensadj@gmail.com>
2024-11-05 22:33:25 +0100

    pedidos

--- routes/pedidosRoutes.js
+++ routes/pedidosRoutes.js
@@ -1,20 +1,17 @@
-// routes/pedidosRoutes.js
 const express = require('express');
+const {
+    crearPedido,
+} = require('../controllers/pedidosController');
+
 const router = express.Router();
-const pedidosController = require('../controllers/pedidosController'); // Asegúrate de que este archivo exporta funciones válidas
 
-// Verifica que las funciones están definidas antes de usarlas en las rutas
-if (pedidosController.getPedidosPorUsuario && pedidosController.crearPedido) {
-  // Ruta para obtener todos los pedidos del usuario autenticado
-  router.get('/', pedidosController.getPedidosPorUsuario);
+// Ruta para obtener todos los pedidos del usuario autenticado
+//router.get('/', getPedidosPorUsuario);
 
-  // Ruta para crear un nuevo pedido
-  router.post('/crear', pedidosController.crearPedido);
+// Ruta para crear un nuevo pedido
+router.post('/crear', crearPedido);
 
-  // Ruta para obtener los detalles de un pedido específico
-  router.get('/:pedidoId', pedidosController.getDetallePedido);
-} else {
-  console.error("Error: Funciones de controlador no definidas en pedidosController");
-}
+// Ruta para obtener los detalles de un pedido específico
+//router.get('/:pedidoId', getDetallePedido);
 
 module.exports = router;
